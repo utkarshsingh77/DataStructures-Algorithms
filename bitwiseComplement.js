24af1b9bf1248d5ee58da82fac298f1f36cc9cb7
Utkarsh Singh <66885923+utkarshsingh77@users.noreply.github.com>
2020-10-05 11:14:24 -0600

    Create bitwiseComplement.js

---
+++ bitwiseComplement.js
@@ -0,0 +1,17 @@
+//Bitwise
+var bitwiseComplement = function(N) {
+    // return N ? (Math.pow(2, Math.ceil(Math.log2(N))) - 1) & ~N : 1;
+};
+
+//Naive
+var bitwiseComplement = function(N) {
+    let binary = N.toString(2);
+    let binaryArr = binary.split('')
+    for (let i = 0; i < binaryArr.length; i++) {
+        if (binaryArr[i] === '1') binaryArr[i] = 0;
+        if (binaryArr[i] === '0') binaryArr[i] = 1;
+    }
+    binaryArr = binaryArr.join('');
+    return parseInt(binaryArr, 2);
+};
+    
