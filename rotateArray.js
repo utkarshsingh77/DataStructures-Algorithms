4b6b312cc973c626bb7147b8e8ba086203c00301
Utkarsh Singh <66885923+utkarshsingh77@users.noreply.github.com>
2020-10-15 02:06:19 -0600

    Update rotateArray.js

--- rotateArray.js
+++ rotateArray.js
@@ -1,11 +1,11 @@
 var rotate = function(nums, k) {
     let resultArr = new Array(nums.length);
-    //From the very last num in nums up to kth from last, add it the beginning of nums
+    //Build result array with the correct index after applying the rotation
     let counter = k;
     for (let i = 0; i < nums.length; i++) {
         resultArr[(i+k) % nums.length] = nums[i]
     }
-    // Now slice off all the elements you added to the beginning above 
+    // Copy resultArr into nums as to rotate nums in place
     for (let i = 0; i < nums.length; i++) {
         nums[i] = resultArr[i];
     }
